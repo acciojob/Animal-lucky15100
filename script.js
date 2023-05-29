9ddd1b637094c0d4dd48c4439607e6c7b1b2e8a7
lucky15100 <131132214+lucky15100@users.noreply.github.com>
2023-05-29 17:43:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const animalPhotos = [
+  "camel",
+  "cheetah",
+  "elephant",
+  "gorilla",
+  "koala",
+  "lion",
+  "polar_bear",
+  "tiger",
+  "wolf"
+];
+
+const container = document.getElementById("animal-photos");
+
+animalPhotos.forEach(animal => {
+  const imageUrl = `https://www2.d125.org/applied_arts/teched/courses/WEB/portfolios/2014_FallP8/Rami_Portfolio_Period8/unit4_css/project_1folder/cssProject1/${animal}.jpg`;
+
+  const animalPhoto = document.createElement("div");
+  animalPhoto.className = "animal-photo";
+
+  const img = document.createElement("img");
+  img.src = imageUrl;
+  img.alt = animal;
+
+  animalPhoto.appendChild(img);
+  container.appendChild(animalPhoto);
+});
